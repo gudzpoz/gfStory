6b4098792662839e6ff12a9b81169cf4193ce6d9
gudzpoz <gudzpoz@live.com>
2023-10-01 01:47:45 +0800

    feat: add help text

--- src/components/LineList.vue
+++ src/components/LineList.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import {
-  NButton, NButtonGroup, NCard, NCollapse, NCollapseItem, NIcon, NSpace, NTag,
+  NButton, NButtonGroup, NCard, NCollapse,
+  NCollapseItem, NIcon, NSpace, NTag,
+  useDialog,
 } from 'naive-ui';
 import {
   AddFilled, ContentPasteFilled, DeleteFilled, DownloadFilled,
+  HelpCenterFilled,
   MoveDownFilled, MoveUpFilled, PeopleFilled, RefreshFilled,
 } from '@vicons/material';
-import { computed, provide, ref } from 'vue';
+import {
+  computed, h, provide, ref,
+} from 'vue';
 
 import CharacterList from './character/CharacterList.vue';
 import StoryLineView from './lines/StoryLineView.vue';
@@ -114,6 +119,23 @@ function canMove(end: number) {
   const i = findIndexByCurrentName();
   return i !== -1 && i !== end;
 }
+
+const dialog = useDialog();
+function showHelpDialog() {
+  dialog.info({
+    content: () => h('ol', {
+      title: '简单使用说明',
+      innerHTML: `
+        <li>使用“编辑角色”按钮来创建角色并添加立绘，需要先添加角色才能在对话中使用立绘。</li>
+        <li>使用“新增节点”按钮来创建剧情对话，也可以用节点来设置背景图片或是背景音乐。</li>
+        <li>使用“暂存并预览”按钮暂存故事，此时在右侧可以预览故事。</li>
+        <li>请务必在刷新页面或是退出浏览器之前使用“暂存并预览”按钮，否则改动将不会被保存。</li>
+        <li>使用“导出故事”按钮将故事导出为一个压缩包，解压后用浏览器打开里面的网页即可直接运行故事。</li>
+        <li>另外，导出的故事应该也可以直接上传到 Itch 上（一个独立游戏发布网站），但我还未测试。</li>
+      `,
+    }),
+  });
+}
 </script>
 
 <template>
@@ -129,23 +151,34 @@ function canMove(end: number) {
       <n-button @click="appendDefaultLine" type="primary">
         <n-icon><add-filled></add-filled></n-icon>添加节点
       </n-button>
-      <n-button @click="copyCurrent" type="warning" :disabled="!canMove(-1)">
-        <n-icon><content-paste-filled></content-paste-filled></n-icon>复制节点
+      <n-button @click="copyCurrent" type="warning" :disabled="!canMove(-1)"
+        title="复制节点"
+      >
+        <n-icon><content-paste-filled></content-paste-filled></n-icon>
       </n-button>
-      <n-button @click="removeLine" type="error" :disabled="!canMove(-1)">
-        <n-icon><delete-filled></delete-filled></n-icon>移除当前
+      <n-button @click="removeLine" type="error" :disabled="!canMove(-1)"
+        title="移除当前"
+      >
+        <n-icon><delete-filled></delete-filled></n-icon>
       </n-button>
-      <n-button @click="moveUp" secondary type="primary" :disabled="!canMove(0)">
-        <n-icon><move-up-filled></move-up-filled></n-icon>上移
+      <n-button @click="moveUp" secondary type="primary" :disabled="!canMove(0)"
+        title="上移"
+      >
+        <n-icon><move-up-filled></move-up-filled></n-icon>
       </n-button>
-      <n-button @click="moveDown" secondary type="primary" :disabled="!canMove(lines.length - 1)">
-        <n-icon><move-down-filled></move-down-filled></n-icon>下移
+      <n-button @click="moveDown" secondary type="primary" :disabled="!canMove(lines.length - 1)"
+        title="下移"
+      >
+        <n-icon><move-down-filled></move-down-filled></n-icon>
       </n-button>
       <n-button @click="emit('update:modelValue', modelValue)" type="warning">
-        <n-icon><refresh-filled></refresh-filled></n-icon>预览故事
+        <n-icon><refresh-filled></refresh-filled></n-icon>暂存并预览
       </n-button>
       <n-button @click="emit('update:modelValue', modelValue); emit('export')" type="primary">
         <n-icon><download-filled></download-filled></n-icon>导出故事
+      </n-button>
+      <n-button @click="showHelpDialog" type="info" title="帮助">
+        <n-icon><help-center-filled></help-center-filled></n-icon>
       </n-button>
     </n-button-group>
   </n-card>
